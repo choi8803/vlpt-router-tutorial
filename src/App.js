4133e473a4037ebebabd8c0e4bed03595a11b003
choi8803 <choijh225@naver.com>
2024-02-16 14:30:53 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+import { Link, Route, Routes, useNavigate } from 'react-router-dom';
 import './App.css';
+import Home from './components/Home';
+import About from './components/About';
+import Profiles from './components/Profiles';
+import NavigateSample from './components/NavigateSample';
 
 function App() {
+  const navigate = useNavigate();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ul>
+        <li>
+          <Link to="/">홈</Link>
+        </li>
+        <li>
+          <Link to="/about">소개</Link>
+        </li>
+        <li>
+          <Link to="/profiles">프로필 목록</Link>
+        </li>
+        <li>
+          <Link to="/navigate">예제</Link>
+        </li>
+      </ul>
+      <hr />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profiles/*" element={<Profiles />} />
+        <Route path="/navigate" element={<NavigateSample />} />
+        <Route path="/*" element={<h1>이 페이지는 존재하지 않습니다. - {navigate.path}</h1>} />
+      </Routes>
     </div>
   );
 }
